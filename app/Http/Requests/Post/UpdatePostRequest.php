ed2578b6d2e4915bfb5cc240c4d23153722a0013
Aref Norouzi <caspiwebinfo@gmail.com>
2023-08-14 21:29:39 +0330

    Post FormRequests

---
+++ app/Http/Requests/Post/UpdatePostRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Post;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdatePostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        /*
+         * add auth()->check() here
+         */
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'bail|required|string|min:2|max:100',
+            'slug' => 'bail|required|string|min:2|max:100|unique:posts,id' . $this->post,
+            'category_id' => 'bail|required|integer|min:1|exists:categories,id'
+        ];
+    }
+}
