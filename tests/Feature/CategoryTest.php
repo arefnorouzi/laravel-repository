0c595c1d24097c5a5a563dd253cd0fa51ff8c456
Aref Norouzi <caspiwebinfo@gmail.com>
2023-08-15 16:01:55 +0330

    improve CategoryTest

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -5,50 +5,56 @@ namespace Tests\Feature;
 use App\Models\Category;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Log;
 use Tests\TestCase;
 
 class CategoryTest extends TestCase
 {
     use RefreshDatabase;
 
+
+    public function test_category_list()
+    {
+        $response = $this->get('/api/category');
+        $response->assertStatus(200);
+    }
+
     public function test_create_category(): void
     {
-        $category = Category::create([
-           'title' => 'test title',
-           'slug' => 'test-category'
-        ]);
-        $this->assertDatabaseHas('categories', [
-            'id' => $category->id,
-            'title' => 'test title'
-        ]);
+        $category = [
+            'title' => 'test title',
+            'slug' => 'test-category'
+        ];
+
+        $response = $this->post('/api/category',$category);
+        $response->assertStatus(201);
     }
 
     public function test_find_category(): void
     {
-        $category = Category::create([
-            'title' => 'find title',
-            'slug' => 'find-category'
-        ]);
+        $category = [
+            'title' => 'test title',
+            'slug' => 'test-category'
+        ];
 
-        $find_item = Category::find($category->id);
+        $this->post('/api/category',$category);
 
-        $this->assertEquals($category->id, $find_item->id);
-        $this->assertEquals($category->title, $find_item->title);
+        $response = $this->get('/api/category/1');
+        $response->assertStatus(200);
     }
 
 
     public function test_delete_category(): void
     {
-        $category = Category::create([
-            'title' => 'delete title',
-            'slug' => 'delete-category'
-        ]);
+        $category = [
+            'title' => 'test title',
+            'slug' => 'test-category'
+        ];
 
-        $category->delete();
+        $this->post('/api/category', $category);
 
-        $this->assertDatabaseMissing('categories', [
-            'id' => $category->id,
-            'title' => 'test title'
-        ]);
+        $response = $this->delete('/api/category/1');
+
+        $response->assertStatus(200);
     }
 }
